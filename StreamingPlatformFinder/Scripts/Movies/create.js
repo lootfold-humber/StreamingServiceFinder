c57d2741801233810ba825913adb6a9049dcb299
Pallav Dubey <pallav.dubey@gmail.com>
2022-02-05 16:30:53 -0500

    refactor movie create & edit js files

--- StreamingPlatformFinder/Scripts/Movies/create.js
+++ StreamingPlatformFinder/Scripts/Movies/create.js
@@ -1,76 +1,79 @@
-﻿const baseUrl = "/api/movies";
-let platforms = [];
+﻿let allPlatforms = [];
+let addForm;
 
-window.onload = function () {
-  getPlatforms();
+window.onload = handleLoad;
 
-  const form = document.forms.addMovie;
-  form.onsubmit = function () {
-    const requestBody = {
-      title: form.title.value,
-      director: form.director.value,
-      genre: form.genre.value,
-      releaseYear: form.releaseYear.value,
-      platformIds: getPlatformValues(),
-    };
+function handleLoad() {
+  getAllPlatforms();
 
-    var xhr = new XMLHttpRequest();
+  addForm = document.forms.addMovie;
+  addForm.onsubmit = handleAddFormSubmit;
+}
 
-    xhr.open("POST", baseUrl, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
-      if (this.status == 201) {
-        window.location.replace("/movies/index");
-      }
-    };
+function getAllPlatforms() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/api/platforms", true);
+  xhr.onload = function () {
+    if (this.status == 200) {
+      allPlatforms = JSON.parse(xhr.response);
+      addCheckboxesForPlatform();
+    }
+  };
+  xhr.send();
+}
 
-    xhr.send(JSON.stringify(requestBody));
-    return false;
+function addCheckboxesForPlatform() {
+  const div = document.querySelector("#platforms");
+  allPlatforms.forEach((p) => {
+    div.innerHTML += createCheckboxForPlatform(p);
+  });
+}
+
+function createCheckboxForPlatform(platform) {
+  return `
+    <input
+      type="checkbox"
+      id="${platform.Name.toLowerCase()}"
+      name="platforms"
+      value="${platform.Id}" />
+    <label
+      class="control-label"
+      for="${platform.Name.toLowerCase()}">
+        ${platform.Name}
+    </label>`;
+}
+
+function handleAddFormSubmit() {
+  const requestBody = {
+    title: addForm.title.value,
+    director: addForm.director.value,
+    genre: addForm.genre.value,
+    releaseYear: addForm.releaseYear.value,
+    platformIds: getPlatformValuesFromAddForm(),
   };
 
-  function getPlatformValues() {
-    const selectedValues = [];
-    const platformElements = form.platforms;
-    for (let i = 0; i < platformElements.length; i++) {
-      const p = platformElements[i];
-      if (p["type"] == "checkbox" && p.checked) {
-        selectedValues.push(parseInt(p.value));
-      }
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "/api/movies", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function () {
+    if (this.status == 201) {
+      window.location.replace("/movies/index");
     }
-    return selectedValues;
-  }
+  };
+  xhr.send(JSON.stringify(requestBody));
 
-  function getPlatforms() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/api/platforms", true);
+  return false;
+}
 
-    xhr.onload = function () {
-      if (this.status == 200) {
-        platforms = JSON.parse(xhr.response);
-        addCheckboxesForPlatform();
-      }
-    };
-    xhr.send();
+function getPlatformValuesFromAddForm() {
+  const selectedValues = [];
+  const platformElements = addForm.platforms;
+  for (let i = 0; i < platformElements.length; i++) {
+    const p = platformElements[i];
+    if (p["type"] == "checkbox" && p.checked) {
+      selectedValues.push(parseInt(p.value));
+    }
   }
-
-  function addCheckboxesForPlatform() {
-    const div = document.querySelector("#platforms");
-    platforms.forEach((p) => {
-      div.innerHTML += createCheckboxForPlatform(p);
-    });
-  }
-
-  function createCheckboxForPlatform(platform) {
-    return `
-      <input
-        type="checkbox"
-        id="${platform.Name.toLowerCase()}"
-        name="platforms"
-        value="${platform.Id}" />
-      <label
-        class="control-label"
-        for="${platform.Name.toLowerCase()}">
-          ${platform.Name}
-      </label>`;
-  }
-};
+  return selectedValues;
+}
--- StreamingPlatformFinder/Scripts/Movies/edit.js
+++ StreamingPlatformFinder/Scripts/Movies/edit.js
@@ -1,119 +1,122 @@
-﻿const baseUrl = "/api/movies";
+﻿let movieId;
 let platforms = [];
-let movieId;
+let editForm;
+let movieToEdit;
 
-window.onload = function () {
-  getPlatforms();
+window.onload = handleLoad;
 
+function handleLoad() {
   const splitArr = window.location.toString().split("/");
   movieId = splitArr[splitArr.length - 1];
 
-  const form = document.forms.editMovie;
-  form.onsubmit = function () {
-    const requestBody = {
-      title: form.title.value,
-      director: form.director.value,
-      genre: form.genre.value,
-      releaseYear: form.releaseYear.value,
-      platformIds: getPlatformValues(),
-    };
+  getAllPlatforms();
 
-    var xhr = new XMLHttpRequest();
+  editForm = document.forms.editMovie;
+  editForm.onsubmit = handleEditFormSubmit;
+}
 
-    xhr.open("PUT", `${baseUrl}/${movieId}`, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
-      if (this.status == 200) {
-        window.location.replace("/movies/index");
-      }
-    };
+function getAllPlatforms() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/api/platforms", true);
 
-    xhr.send(JSON.stringify(requestBody));
-    return false;
+  xhr.onload = function () {
+    if (this.status == 200) {
+      allPlatforms = JSON.parse(xhr.response);
+      addCheckboxesForPlatform();
+      getMovieToEdit();
+    }
+  };
+  xhr.send();
+}
+
+function addCheckboxesForPlatform() {
+  const div = document.querySelector("#platforms");
+  allPlatforms.forEach((p) => {
+    div.innerHTML += createCheckboxInpForPlatform(p);
+  });
+}
+
+function createCheckboxInpForPlatform(platform) {
+  return `
+    <input
+      type="checkbox"
+      id="${platform.Name.toLowerCase()}"
+      name="platforms"
+      value="${platform.Id}" />
+    <label
+      class="control-label"
+      for="${platform.Name.toLowerCase()}">
+        ${platform.Name}
+    </label>`;
+}
+
+function getMovieToEdit() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `/api/movies/${movieId}`, true);
+  xhr.onload = function () {
+    if (this.status == 200) {
+      movieToEdit = JSON.parse(xhr.response);
+      addMovieDetailsToPage();
+    }
+  };
+  xhr.send();
+}
+
+function addMovieDetailsToPage() {
+  const titleTxt = document.querySelector("#title");
+  const directorTxt = document.querySelector("#director");
+  const genreTxt = document.querySelector("#genre");
+  const yearTxt = document.querySelector("#releaseYear");
+  const platformsInp = document.querySelectorAll('[name="platforms"]');
+
+  titleTxt.value = movieToEdit.Title;
+  directorTxt.value = movieToEdit.Director;
+  genreTxt.value = movieToEdit.Genre;
+  yearTxt.value = movieToEdit.ReleaseYear;
+
+  const platformIds = movieToEdit.Platforms.map((p) => p.Id);
+
+  for (let i = 0; i < platformsInp.length; i++) {
+    const checkBox = platformsInp[i];
+    const checkBoxValue = parseInt(checkBox["value"]);
+    if (platformIds.includes(checkBoxValue)) {
+      checkBox.checked = true;
+    }
+  }
+}
+
+function handleEditFormSubmit() {
+  const requestBody = {
+    title: editForm.title.value,
+    director: editForm.director.value,
+    genre: editForm.genre.value,
+    releaseYear: editForm.releaseYear.value,
+    platformIds: getSelectedPlatformsFromEditForm(),
   };
 
-  function getPlatformValues() {
-    const selectedValues = [];
-    const platformElements = form.platforms;
-    for (let i = 0; i < platformElements.length; i++) {
-      const p = platformElements[i];
-      if (p["type"] == "checkbox" && p.checked) {
-        selectedValues.push(parseInt(p.value));
-      }
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("PUT", `/api/movies/${movieId}`, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function () {
+    if (this.status == 200) {
+      window.location.replace("/movies/index");
     }
-    return selectedValues;
-  }
+  };
 
-  function addMovieToPage() {
-    const titleTxt = document.querySelector("#title");
-    const directorTxt = document.querySelector("#director");
-    const genreTxt = document.querySelector("#genre");
-    const yearTxt = document.querySelector("#releaseYear");
-    const platformsInp = document.querySelectorAll('[name="platforms"]');
+  xhr.send(JSON.stringify(requestBody));
+  return false;
+}
 
-    titleTxt.value = movie.Title;
-    directorTxt.value = movie.Director;
-    genreTxt.value = movie.Genre;
-    yearTxt.value = movie.ReleaseYear;
-
-    const platformIds = movie.Platforms.map((p) => p.Id);
-
-    for (let i = 0; i < platformsInp.length; i++) {
-      const checkBox = platformsInp[i];
-      const checkBoxValue = parseInt(checkBox["value"]);
-      if (platformIds.includes(checkBoxValue)) {
-        checkBox.checked = true;
-      }
+function getSelectedPlatformsFromEditForm() {
+  const selectedValues = [];
+  const platformInputs = document.querySelectorAll('[name="platforms"]');
+  for (let i = 0; i < platformInputs.length; i++) {
+    const inp = platformInputs[i];
+    const inpValue = parseInt(inp.value);
+    if (inp.checked) {
+      selectedValues.push(inpValue);
     }
   }
-
-  function getMovie() {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", `${baseUrl}/${movieId}`, true);
-
-    xhr.onload = function () {
-      if (this.status == 200) {
-        movie = JSON.parse(xhr.response);
-        addMovieToPage();
-      }
-    };
-
-    xhr.send();
-  }
-
-  function getPlatforms() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/api/platforms", true);
-
-    xhr.onload = function () {
-      if (this.status == 200) {
-        platforms = JSON.parse(xhr.response);
-        addCheckboxesForPlatform();
-        getMovie();
-      }
-    };
-    xhr.send();
-  }
-
-  function addCheckboxesForPlatform() {
-    const div = document.querySelector("#platforms");
-    platforms.forEach((p) => {
-      div.innerHTML += createCheckboxForPlatform(p);
-    });
-  }
-
-  function createCheckboxForPlatform(platform) {
-    return `
-      <input
-        type="checkbox"
-        id="${platform.Name.toLowerCase()}"
-        name="platforms"
-        value="${platform.Id}" />
-      <label
-        class="control-label"
-        for="${platform.Name.toLowerCase()}">
-          ${platform.Name}
-      </label>`;
-  }
-};
+  return selectedValues;
+}
