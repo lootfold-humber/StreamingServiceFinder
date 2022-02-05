36e39f12db2625ba424abbce03fca102a9dd60b5
Pallav Dubey <pallav.dubey@gmail.com>
2022-02-05 03:29:18 -0500

    add edit platform page

--- StreamingPlatformFinder/Controllers/PlatformsController.cs
+++ StreamingPlatformFinder/Controllers/PlatformsController.cs
@@ -13,5 +13,10 @@ namespace StreamingPlatformFinder.Controllers
         {
             return View();
         }
+        
+        public ActionResult Edit()
+        {
+            return View();
+        }
     }
 }
---
+++ StreamingPlatformFinder/Scripts/Platforms/edit.js
@@ -0,0 +1,49 @@
+﻿const baseUrl = "/api/platforms";
+let platformId;
+
+window.onload = function () {
+  const splitArr = window.location.toString().split("/");
+  platformId = splitArr[splitArr.length - 1];
+
+  const form = document.forms.editPlatform;
+  form.onsubmit = function () {
+    const requestBody = {
+      name: form.name.value,
+    };
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("PUT", `${baseUrl}/${platformId}`, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+      if (this.status == 200) {
+        window.location.replace("/platforms/index");
+      }
+    };
+
+    xhr.send(JSON.stringify(requestBody));
+    return false;
+  };
+
+  getPlatform();
+
+  function addPlatformToPage() {
+    const nameTxt = document.querySelector("#name");
+    nameTxt.value = platform.Name;
+  }
+
+  function getPlatform() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `${baseUrl}/${platformId}`, true);
+
+    xhr.onload = function () {
+      if (this.status == 200) {
+        platform = JSON.parse(xhr.response);
+        addPlatformToPage();
+      }
+    };
+
+    xhr.send();
+  }
+};
