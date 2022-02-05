f4b3b627b1d8ff119d6abd3e70dd0377a79d6161
Pallav Dubey <pallav.dubey@gmail.com>
2022-02-05 16:38:00 -0500

    refactor movie index js

--- StreamingPlatformFinder/Scripts/Movies/index.js
+++ StreamingPlatformFinder/Scripts/Movies/index.js
@@ -1,115 +1,114 @@
-﻿const baseUrl = "/api/movies";
-let moviesArray = [];
+﻿let movies = [];
 let pageNo = 1;
 let pageSize = 5;
 let totalPages = 0;
 let searchKey = null;
 
-window.onload = function () {
-  const searchBox = document.querySelector("#searchBox");
-  const prevBtn = document.querySelector("#btnPrev");
-  const nextBtn = document.querySelector("#btnNext");
+let searchBox;
+let prevBtn;
+let nextBtn;
+
+window.onload = handleLoad;
+
+function handleLoad() {
+  searchBox = document.querySelector("#searchBox");
+  prevBtn = document.querySelector("#btnPrev");
+  nextBtn = document.querySelector("#btnNext");
 
   searchBox.onkeyup = searchMovie;
-
   prevBtn.onclick = prevPage;
   nextBtn.onclick = nextPage;
 
-  checkPrevPageBtn();
-  checkNextPageBtn();
+  getAllMovies();
+  updatePrevBtnState();
+  updateNextBtnState();
+}
 
-  getMovies();
+function getAllMovies() {
+  var xhr = new XMLHttpRequest();
 
-  function searchMovie() {
-    if (searchBox.value.length >= 3) {
-      searchKey = searchBox.value;
-      getMovies();
+  let url = `/api/movies?pageNo=${pageNo}&pageSize=${pageSize}`;
+  if (searchKey) url += `&searchKey=${searchKey}`;
+
+  xhr.open("GET", url, true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      const response = JSON.parse(xhr.response);
+      movies = response.Movies;
+      totalPages = Math.ceil(response.Count / pageSize);
+      clearTbody();
+      updateTbody();
+      updatePrevBtnState();
+      updateNextBtnState();
     }
+  };
 
-    if (searchBox.value.length < 3 && searchKey != null) {
-      searchKey = null;
-      getMovies();
-    }
+  xhr.send();
+}
+
+function clearTbody() {
+  const tableBody = document.querySelector("#movieTable tbody");
+  tableBody.innerHTML = "";
+}
+
+function updateTbody() {
+  const tableBody = document.querySelector("#movieTable tbody");
+  movies.forEach((movie) => {
+    const tr = createNewTr(movie);
+    tableBody.appendChild(tr);
+  });
+}
+
+function createNewTr(movie) {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>
+      <a href="/movies/show/${movie.Id}">${movie.Title}</a>
+    </td>
+    <td>${movie.Director}</td>
+    <td>${movie.Genre}</td>
+    <td>${movie.ReleaseYear}</td>`;
+
+  return tr;
+}
+
+function searchMovie() {
+  if (searchBox.value.length >= 3) {
+    searchKey = searchBox.value;
+    getAllMovies();
   }
 
-  function prevPage() {
-    pageNo--;
-    getMovies();
-    checkPrevPageBtn();
-    checkNextPageBtn();
+  if (searchBox.value.length < 3 && searchKey != null) {
+    searchKey = null;
+    getAllMovies();
   }
+}
 
-  function nextPage() {
-    pageNo++;
-    getMovies();
-    checkPrevPageBtn();
-    checkNextPageBtn();
-  }
+function prevPage() {
+  pageNo--;
+  getAllMovies();
+}
 
-  function checkPrevPageBtn() {
-    if (pageNo === 1) disableBtn(prevBtn);
-    else enableBtn(prevBtn);
-  }
+function nextPage() {
+  pageNo++;
+  getAllMovies();
+}
 
-  function checkNextPageBtn() {
-    if (pageNo === totalPages) disableBtn(nextBtn);
-    else enableBtn(nextBtn);
-  }
+function updatePrevBtnState() {
+  if (pageNo === 1) disableBtn(prevBtn);
+  else enableBtn(prevBtn);
+}
 
-  function disableBtn(btn) {
-    btn.disabled = true;
-  }
+function updateNextBtnState() {
+  if (pageNo === totalPages) disableBtn(nextBtn);
+  else enableBtn(nextBtn);
+}
 
-  function enableBtn(btn) {
-    btn.disabled = false;
-  }
+function disableBtn(btn) {
+  btn.disabled = true;
+}
 
-  function updateTbody() {
-    const tableBody = document.querySelector("#movieTable tbody");
-    moviesArray.forEach((movie) => {
-      const tr = createNewTr(movie);
-      tableBody.appendChild(tr);
-    });
-  }
-
-  function createNewTr(movie) {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>
-        <a href="/movies/show/${movie.Id}">${movie.Title}</a>
-      </td>
-      <td>${movie.Director}</td>
-      <td>${movie.Genre}</td>
-      <td>${movie.ReleaseYear}</td>`;
-
-    return tr;
-  }
-
-  function clearTbody() {
-    const tableBody = document.querySelector("#movieTable tbody");
-    tableBody.innerHTML = "";
-  }
-
-  function getMovies() {
-    var xhr = new XMLHttpRequest();
-
-    let url = `${baseUrl}?pageNo=${pageNo}&pageSize=${pageSize}`;
-    if (searchKey) url += `&searchKey=${searchKey}`;
-
-    xhr.open("GET", url, true);
-
-    xhr.onload = function () {
-      if (this.status == 200) {
-        const response = JSON.parse(xhr.response);
-        moviesArray = response.Movies;
-        totalPages = Math.ceil(response.Count / pageSize);
-        clearTbody();
-        updateTbody();
-        checkPrevPageBtn();
-        checkNextPageBtn();
-      }
-    };
-
-    xhr.send();
-  }
-};
+function enableBtn(btn) {
+  btn.disabled = false;
+}
